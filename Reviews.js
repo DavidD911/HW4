7a7a6dbb668b45226ab411b44f58434f5fced8e4
David Desrochers <dave@coloradocis.com>
2023-04-10 10:59:24 -0600

    latest

--- Reviews.js
+++ Reviews.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 // Movie schema
 var ReviewSchema = new Schema({
   userID: {type: Schema.Types.ObjectId, ref: "User Schema", required: true},
-  movieID: {type: Schema.Types.ObjectId, ref: "Movie Schema", required: true},
+  movieID: {type: Schema.Types.ObjectId, ref: "Movie", required: true},
   username: {type: String, required: true},
   quote: {type: String, required: true},
   rating: {type: Number, min: 1, max: 5, required: true}
